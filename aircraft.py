548ac90f37142693941dfe53c8632f395c3f04b9
MooseZ690 <161773562+MooseZ690@users.noreply.github.com>
2025-05-15 13:54:11 +1200

    Added the start of the user interface

--- aircraft.py
+++ aircraft.py
@@ -7,46 +7,56 @@ manufacturerid = 1 #defaults to the first manufacturer, in this case boeing
 DATABASE = 'aircraft.db'
 
 def print_by_speed():
-    f"""
+    cursor = f"""
     SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id 
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
     ORDER by aircraft.top_speed_kmh {direction}
     """
 def print_by_g():
-    f"""
+    cursor = f"""
     SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id 
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
     ORDER by aircraft.top_speed_kmh {direction}
     """
 def print_by_payload():
-    f"""
+    cursor = f"""
     SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id 
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
     ORDER by aircraft.top_speed_kmh {direction}
     """
 def print_by_climb_rate():
-    f"""
+    cursor = f"""
     SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id 
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
     ORDER by aircraft.top_speed_kmh {direction}
     """
 def print_by_manufacturer():
-    f"""
+    cursor = f"""
     SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id 
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
     WHERE manufacturer.manufacturer_id = {manufacturerid}
     """
 def print_by_country():
-    f"""
+    cursor = f"""
     SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id 
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
     WHERE country.country_id = {countryid}
     """
 def main():
-    print('hello')
+    print('Super duper cool aircraft database')
+    print(
+"""Available commands:
+Sort by speed - '1'
+Sort by g - '2'
+Sort by payload - '3'
+Sort by climb rate - '4'
+Sort by manufacturer - '5'
+Sort by country - '6'
+"""
+    )
