fe357e404c0761059cc7418f901882c05a802ce7
MooseZ690 <161773562+MooseZ690@users.noreply.github.com>
2025-05-16 12:11:40 +1200

    added functions to assign to the buttons when they are implemented

--- aircraft.py
+++ aircraft.py
@@ -1,29 +1,36 @@
-import tkinter as tk
+import tkinter as tk #imports the module for creating windows
 import sqlite3
 from tkinter import messagebox
 
+sql = ''  #initialize the sql variable
 DATABASE = "aircraft.db"
 
-def fetch_and_print(sql, parameters=()):
+def fetch_and_print(sql):
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
-        cursor.execute(sql, parameters)
+        results = cursor.execute(sql)
+    return results
 
 def print_by_speed():
-
+    global sql
+    sql = f'{BASE_SELECT}\n ORDER BY aircraft.top_speed_kmh DESC'
+    fetch_and_print(sql)
 def print_by_g_limit():
-
+    global sql
+    sql = f'{BASE_SELECT}\n ORDER BY aircraft.g_limit DESC'
+    fetch_and_print(sql)
 def print_by_payload():
-
+    global sql
+    sql = f'{BASE_SELECT}\n ORDER BY aircraft.payload_lbs DESC'
+    fetch_and_print(sql)
 def print_by_climb_rate():
+    global sql
+    sql = f'{BASE_SELECT}\n ORDER BY aircraft.climb_rate_fpm DESC'
+    fetch_and_print(sql)
 
-
-def display_text(results):
-    
 
 BASE_SELECT = """
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs,
-           aircraft.climb_rate_fpm, manufacturer.manufacturer_name
+    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name
     FROM aircraft
     INNER JOIN country ON aircraft.country = country.country_id
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
