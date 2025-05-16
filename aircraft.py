944c4039bc3aa089522a69eb69c4a43a2d2a245c
MooseZ690 <161773562+MooseZ690@users.noreply.github.com>
2025-05-16 12:05:41 +1200

    started on tkinter implementation

--- aircraft.py
+++ aircraft.py
@@ -1,62 +1,55 @@
+import tkinter as tk
 import sqlite3
+from tkinter import messagebox
 
-direction = 'DESC' #defaults to descending order on specifics
-countryid = 1 #defaults to first country, in this case america as the majority of aircraft are american
-manufacturerid = 1 #defaults to the first manufacturer, in this case boeing
+DATABASE = "aircraft.db"
 
-DATABASE = 'aircraft.db'
+def fetch_and_print(sql, parameters=()):
+    with sqlite3.connect(DATABASE) as db:
+        cursor = db.cursor()
+        cursor.execute(sql, parameters)
 
 def print_by_speed():
-    cursor = f"""
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
-    INNER JOIN country ON aircraft.country = country.country_id 
-    INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
-    ORDER by aircraft.top_speed_kmh {direction}
-    """
-def print_by_g():
-    cursor = f"""
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
-    INNER JOIN country ON aircraft.country = country.country_id 
-    INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
-    ORDER by aircraft.top_speed_kmh {direction}
-    """
+
+def print_by_g_limit():
+
 def print_by_payload():
-    cursor = f"""
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
-    INNER JOIN country ON aircraft.country = country.country_id 
-    INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
-    ORDER by aircraft.top_speed_kmh {direction}
-    """
+
 def print_by_climb_rate():
-    cursor = f"""
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
-    INNER JOIN country ON aircraft.country = country.country_id 
+
+
+def display_text(results):
+    
+
+BASE_SELECT = """
+    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs,
+           aircraft.climb_rate_fpm, manufacturer.manufacturer_name
+    FROM aircraft
+    INNER JOIN country ON aircraft.country = country.country_id
     INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
-    ORDER by aircraft.top_speed_kmh {direction}
-    """
-def print_by_manufacturer():
-    cursor = f"""
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
-    INNER JOIN country ON aircraft.country = country.country_id 
-    INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
-    WHERE manufacturer.manufacturer_id = {manufacturerid}
-    """
-def print_by_country():
-    cursor = f"""
-    SELECT aircraft.aircraft_name, aircraft.top_speed_kmh, aircraft.g_limit, aircraft.payload_lbs, aircraft.climb_rate_fpm, manufacturer.manufacturer_name FROM aircraft
-    INNER JOIN country ON aircraft.country = country.country_id 
-    INNER JOIN manufacturer ON aircraft.manufacturer = manufacturer.manufacturer_id
-    WHERE country.country_id = {countryid}
-    """
-def main():
-    print('Super duper cool aircraft database')
-    print(
-"""Available commands:
-Sort by speed - '1'
-Sort by g - '2'
-Sort by payload - '3'
-Sort by climb rate - '4'
-Sort by manufacturer - '5'
-Sort by country - '6'
 """
-    )
+
+root = tk.Tk()
+root.title("Aircraft Database")
+root.geometry("800x600")
+root.resizable(False, False)
+
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
+
+
+speedbutton = tk.Button(root, text="Sort by Speed", command=print_by_speed)
+
+
+gbutton = tk.Button(root, text="Sort by G Limit", command=print_by_g_limit)
+
+
+payloadbutton = tk.Button(root, text="Sort by Payload", command=print_by_payload)
+
+
+climbbutton = tk.Button(root, text="Sort by Climb Rate", command=print_by_climb_rate)
+
+
+
+
+root.mainloop()
---
+++ test.py
@@ -0,0 +1,8 @@
+def main():
+    print('Super duper cool aircraft database')
+    print('Input selected command:')
+    print("Sort by: \nSpeed - '1' \nMax G tolerance - '2' \nPayload - '3' \nClimb rate - '4' \nManufacturer - '5' \nCountry - '6'\n")
+    sortby = input('[>]')
+   
+    
+main()
