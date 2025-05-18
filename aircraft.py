86a579ea2cd595d9ed0a8cbe1d518ba13897a24b
MooseZ690 <161773562+MooseZ690@users.noreply.github.com>
2025-05-19 11:45:58 +1200

    cleaned up code visually, removed unnecessary lines (e.g duplicates), made the text box editable IN the output function instead of just printing one value it prints them all then becomes uneditabe.

--- aircraft.py
+++ aircraft.py
@@ -19,9 +19,15 @@ def fetch_and_print(sql):
     results = cursor.fetchall()
     output_text.configure(state="normal") #makes the text box editable just while inserting resylts
     for skibidi in results: #results is a list of tuples, which are like lists but unchangeable
+        
         output_text.insert(tk.END, f"Aircraft Name: {skibidi[0]}\n")
         output_text.insert(tk.END, f"Top speed: {skibidi[1]}km/h\n")
-        output_text.configure(state="disabled") #makes the text box uneditable again
+        output_text.insert(tk.END, f"G Limit: {skibidi[2]}\n")
+        output_text.insert(tk.END, f"Payload: {skibidi[3]}lbs\n")
+        output_text.insert(tk.END, f"Climb Rate: {skibidi[4]}fpm\n")
+        output_text.insert(tk.END, f"Manufacturer: {skibidi[5]}\n")
+        output_text.insert(tk.END, "------------------------\n")
+    output_text.configure(state="disabled") #makes the text box uneditable again
 
 def print_by_speed():
     global sql
@@ -47,23 +53,22 @@ root.title("Aircraft Database") #names the window
 root.geometry("668x600") #makes the window 668x600, just enough to fit the text box
 
 
-speedbutton = tk.Button(root, text="Sort by Speed", command=print_by_speed) #creates a button in root that runs the print_by_speed. 
+speedbutton = tk.Button(root, text="Sort by Speed", command = print_by_speed) #creates a button in root that runs the print_by_speed. 
 speedbutton.grid(row=0, column=0, padx=10, pady=10)
 
-gbutton = tk.Button(root, text="Sort by G Limit", command=print_by_g_limit) #creates a button in root that runs the print_by_g_limit.
+gbutton = tk.Button(root, text="Sort by G Limit", command = print_by_g_limit) #creates a button in root that runs the print_by_g_limit.
 gbutton.grid(row=0, column=1, padx=10, pady=10)
 
-payloadbutton = tk.Button(root, text="Sort by Payload", command=print_by_payload) #creates a button in root that runs the print_by_payload.
+payloadbutton = tk.Button(root, text="Sort by Payload", command = print_by_payload) #creates a button in root that runs the print_by_payload.
 payloadbutton.grid(row=0, column=2, padx=10, pady=10)
 
-climbbutton = tk.Button(root, text="Sort by Climb Rate", command=print_by_climb_rate) #creates a button in root that runs the print_by_climb_rate.
+climbbutton = tk.Button(root, text="Sort by Climb Rate", command = print_by_climb_rate) #creates a button in root that runs the print_by_climb_rate.
 climbbutton.grid(row=0, column=3, padx=10, pady=10)
 
 output_text = tk.Text(root)
 output_text.grid(row=1, column=0, columnspan=4, rowspan=10, padx=10, pady=20) #makes the text box as wide as all the buttons, just below them
-output_text.configure(state="disabled") #makes the text box only output, so the use cant type in it
-#i cant use pack because you cant have grid and pack in the same window
-output_text.delete("1.0", tk.END) #idk why but apparently i need to do this to clear preivious responses
+output_text.configure(state="disabled") #makes the text box only output, so the user cant type in it
+#i cant use pack because you cant use grid and pack for formatting in the same window
 
 
 root.mainloop()
